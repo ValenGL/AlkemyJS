6f241c14dd5155cb9ebd81e22e7287e9ecc71805
Valentin <valentingonzalezlou@gmail.com>
2021-09-17 19:59:39 -0300

    arregla sequalize deja funcionando db

--- server/config/default.js
+++ server/config/default.js
@@ -1,11 +1,11 @@
-require ('dotenv').config();
+require("dotenv").config();
 
-module.exports ={
-  mysql:{
-    host: 'localhost',
+module.exports = {
+  mysql: {
+    host: "localhost",
     port: 3306,
-    db:process.env.MYSQL_DB_ENV,
+    db: process.env.MYSQL_DB_ENV,
     username: process.env.MYSQL_USER_ENV,
-    password: process.env.MYSQL_PASSWORD_ENV
-  }
-}
+    password: process.env.MYSQL_PASSWORD_ENV,
+  },
+};
